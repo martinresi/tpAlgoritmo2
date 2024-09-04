ff7a5276c4ad9d34cf41b612a07501c4fae81826
MartinCatalanoResi <96340292+martinresi@users.noreply.github.com>
2024-09-03 22:54:39 -0300

    Update tpLogico.pl
    base de conocimiento

---
+++ tpLogico.pl
@@ -0,0 +1,17 @@
+persona(samuel, lentes(negros)).
+persona(samuel, pelo(blanco,lacio)).
+persona(samuel, calvo).
+persona(samuel, boca(chica)).
+persona(samuel, cara(puntuda)).
+persona(samuel,ojos(marrones)).
+persona(samuel, nariz(chica)).
+persona(pepe, lentes(rojos)).
+persona(pepe, pelo(rubio,rulos)).
+persona(pepe, boca(chica)).
+persona(pepe, cara(puntuda)).
+persona(pepe, ojos(marrones)).
+persona(pepe, nariz(chica)).
+carta(rojo, samuel).
+carta(azul, pepe).
+contrincante(rojo, azul).
+contrincante(azul, rojo).
