f14b4c95141353fad1805fa90d1bebca304e033e
martin <martin catalano>
2024-10-29 11:57:34 -0300

    se realizo punto 1, 2( a y b )

--- tpHaskell/app/Main.hs
+++ tpHaskell/app/Main.hs
@@ -2,5 +2,30 @@ module Main (main) where
 
 import Lib
 
+-- 1
+
+data Mago = Mago{nombre::String, edad::Int, salud::Int, hechizos::[Hechizos]}
+
+data Hechizos = LagrimaFenix {lagrimaFenixSalud::Int}
+              | SectumSempra {sectumSempraDanio::Int}
+              | Obliviate {}
+              | Confundus {}
+              deriving (Show, Eq)
+
+--2
+--a
+poder::Mago -> Int
+poder mago = salud mago + (edad mago * length (hechizos mago))
+--b
+danio:: Mago -> Hechizos -> Int
+-- a la salud del magoo total   le resto la que se cura  ATTENTION : a testear
+danio mago (LagrimaFenix lagrimaFenixSalud) = salud mago - ( salud mago + lagrimaFenixSalud )   
+
+
+--c
+-- diferenciaDePoder:: Mago -> Mago -> Int
+
+
+--3
 main :: IO ()
 main = someFunc
