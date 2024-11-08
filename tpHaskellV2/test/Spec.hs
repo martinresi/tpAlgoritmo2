7ea4f25f555723232fbb65e6ff64a3c2685fc0d7
MartinCatalanoResi <96340292+martinresi@users.noreply.github.com>
2024-11-08 13:28:46 -0300

    Delete tpHaskellV2 directory

--- tpHaskellV2/test/Spec.hs
+++
@@ -1,8 +0,0 @@
-import Test.Hspec
-import Lib
-
-main :: IO ()
-main = hspec $ do
-    describe "poder" $ do
-        it "Calcula el poder de un mago con hechizos" $ do
-        poder harry `shouldBe` 110
