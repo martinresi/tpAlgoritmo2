3e02d10837bcbf64a7e334f5bfc372d1f7541c54
Snavzen <165828527+Snavzen@users.noreply.github.com>
2024-11-11 11:36:11 -0300

    Cambios_punto_3

--- tests.pl
+++ tests.pl
@@ -55,7 +55,7 @@ test("Samuel no es rubio de boca chica", fail) :-
 :- end_tests(rubiosDeBocaChica).
 
 :- begin_tests(verificar_Inversibilidad).
-test("conjunto_de_rubios_con_boca_chica", set(Personas == [juan, pepe])) :- rubiosDeBocaChica(Personas).
+test("conjunto_de_rubios_con_boca_chica", set(Personas == [[juan, pepe]])) :- rubiosDeBocaChica(Personas).
 :- end_tests(verificar_Inversibilidad).
 
 % PUNTO 4
