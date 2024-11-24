a7046a650439cc2f3ad8195cebbd3f6e9fe13c4f
martin <martin catalano>
2024-11-24 12:53:49 -0300

    andan todos los test

--- tpHaskellV2/test/Spec.hs
+++ tpHaskellV2/test/Spec.hs
@@ -20,16 +20,17 @@ main = hspec $ do
     describe "obliviate" $ do
 
         it "No elimina ningún hechizo si n es 0" $
-            hechizos (obliviate 0 ron) `shouldBe` [confundus]
+            length (hechizos (obliviate 0 ron)) `shouldBe` 1
 
         it "Elimina los primeros n hechizos del mago" $
-            length (hechizos (obliviate 1 luis)) shouldBe 1
+            (length (hechizos (obliviate 1 luis))) `shouldBe` 1
 
         it "Elimina todos los hechizos si n es igual a la cantidad de hechizos" $
-            length (hechizos (obliviate 10 mark)) shouldBe 0
+            (length (hechizos (obliviate 10 mark))) `shouldBe` 0
 
         it "No elimina ningún hechizo si n es 0" $
-            length (hechizos (obliviate 0 ron)) shouldBe 1
+            (length (hechizos (obliviate 0 ron))) `shouldBe` 1
+
 
     describe "confundus" $ do
         it "Hechizo que reduce la salud del mago en 5 si tiene al menos un hechizo" $
